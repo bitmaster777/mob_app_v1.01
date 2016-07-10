0dee2e646b579bb7ab25c93b8b64e8c790deb0c9
student <student@K0213.step.lan>
2016-07-10 17:49:29 +0300

    Первая фиксация - заготовка проекта, 10.07.2016 17:48

---
+++ src/iorders_v1/pkg01/IOrders_v101.java
@@ -0,0 +1,9 @@
+package iorders_v1.pkg01;
+
+public class IOrders_v101 {
+
+    public static void main(String[] args) {
+        // 10.07.2016 - День начала раздумий над новым проектом.
+    }
+    
+}
